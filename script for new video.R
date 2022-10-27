b3b52a918dedcb8acf37d80b9bab3d73521386ff
Sam <s.dumble@stats4sd.org>
2022-10-27 17:21:00 +0100

    2022 updates

---
+++ script for new video.R
@@ -0,0 +1,15 @@
+#Video script:
+  
+#1 Download R and Rstudio
+#2 Install R before installing RStudio
+#3 Open R (command line) to show "the past"
+#4 Open RStudio - briefly show pane layouts 
+#5 write and save 2+2 script
+#6 Download module zip file https://github.com/stats4sd/r2020_04RStudio/raw/main/Module-2-Solutions.zip
+#7 Extract files to folder ->set up project
+#8 Open rmd file. Show vs script file - run existing chunks + write comments
+#9 install packages
+#10 Show data in environment and spreadsheet mode - briefly cover 
+#11 add a new chunk (2+2)
+#12 save files
+#13 knit
